b27820421820a07cb65f77f2b4c4e29a1a8e84c9
Nelson <juniormoncada17@gmail.com>
2022-11-05 19:31:04 +0100

    test user can login in the app

--- tests/Controller/LoginControllerTest.php
+++ tests/Controller/LoginControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LoginControllerTest extends WebTestCase
@@ -12,5 +13,20 @@ class LoginControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/login');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function test_user_can_login(){
+
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('a@gmail.com');
+
+        $client->loginUser($testUser);
+
+        $client->request('GET', '/home');
+
+        $this->assertResponseIsSuccessful();
+        
     }
 }
