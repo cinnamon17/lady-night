1819f7aaaa460d8394ffd1d42ca0a320bce0595c
Nelson Moncada <juniormoncada17@gmail.com>
2023-07-08 17:13:52 +0200

    websocket live chat in home controller

--- src/Command/RunWebsocketServerCommand.php
+++ src/Command/RunWebsocketServerCommand.php
@@ -2,13 +2,16 @@
 
 namespace App\Command;
 
+use MessageHandler;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'run:websocket-server',
@@ -26,18 +29,18 @@ class RunWebsocketServerCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $port = 3001;
+        $output->writeln("Starting server on port " . $port);
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new MessageHandler()
+                )
+            ),
+            $port
+        );
+        $server->run();
+        return 0;
 
         return Command::SUCCESS;
     }
