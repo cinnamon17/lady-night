e92f441030d0b6ceddc14d3b12a28422192b58af
Nelson Moncada <juniormoncada17@gmail.com>
2023-07-08 15:38:10 +0200

    store stream in buffer variable

--- assets/src/Camera.js
+++ assets/src/Camera.js
@@ -19,10 +19,16 @@ export default class Camera {
 
     async startStream(constraints){
 
+        let buffer = [];
         if(this.video != null){
         this.requestUserPermission();
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        this.video.srcObject = stream;
+            this.video.srcObject = stream;
+
+            const videoTrack = stream.getVideoTracks();
+            const audioTrack = stream.getAudioTracks();
+
+            console.log(videoTrack[0]);
 
         }
     }
